f05f48774526d0cbddd5267320e96b4f6fb3d22a
Julien <julien.maupetit@epsi.fr>
2016-04-29 17:43:16 +0200

    Delete first commit.

--- Rotate-Cube/public/assets/js/Plane.js
+++
@@ -1,8 +0,0 @@
-var Plane = function(){
-    this.geometry = new THREE.PlaneBufferGeometry( 200, 200 );
-    this.geometry.rotateX( - Math.PI / 2 );
-
-    this.material = new THREE.MeshBasicMaterial( { color: 0xe0e0e0, overdraw: 0.5 } );
-
-    this.model = new THREE.Mesh(this.geometry, this.material);
-}
